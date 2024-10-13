08ff70625ac1a47539f2c76853c40e78b8ec521e
Francisca Rosa <f.rosa96@hotmail.com>
2024-10-13 15:31:54 -0300

    feat: first click is never a bomb

--- src/partida.js
+++ src/partida.js
@@ -1,16 +1,20 @@
-function createBoard(size, numMines) {
+const isInBoardSize = (x, y, size) => x >= 0 && x < size && y >= 0 && y < size;
+
+const createBoard = (size, numMines, firstX, firstY) => {
   let board = Array(size)
     .fill()
     .map(() => Array(size).fill(0));
 
-  const isInBoardSize = (x, y) => x >= 0 && x < size && y >= 0 && y < size;
-
   let leftMines = numMines;
+
   while (leftMines > 0) {
     let x = Math.floor(Math.random() * size);
     let y = Math.floor(Math.random() * size);
 
-    if (board[x][y] !== "M") {
+    const canPutMineOnCell =
+      board[x][y] !== "M" && firstX !== x && firstY !== y;
+
+    if (canPutMineOnCell) {
       board[x][y] = "M";
       leftMines--;
     }
@@ -27,7 +31,7 @@ function createBoard(size, numMines) {
           if (dx === 0 && dy === 0) continue;
           let newX = i + dx;
           let newY = j + dy;
-          if (isInBoardSize(newX, newY) && board[newX][newY] === "M") {
+          if (isInBoardSize(newX, newY, size) && board[newX][newY] === "M") {
             closeMines++;
           }
         }
@@ -38,30 +42,49 @@ function createBoard(size, numMines) {
   }
 
   return board;
-}
+};
 
-function renderBoard(board) {
+const defineCellProps = (cell) => {
+  cell.style.color = "black";
+  cell.style.border = "1px solid #000";
+  cell.style.alignItems = "center";
+
+  if (cell.classList.contains("revealed")) {
+    cell.style.backgroundColor = "lightgray";
+  }
+  if (cell.classList.contains("mine")) {
+    cell.innerText = "M";
+    cell.style.backgroundColor = "red";
+  }
+};
+let isFirstClick = true;
+
+const renderBoard = (size, numMines) => {
   const boardDiv = document.querySelector(".board");
-  const L = board.length;
 
   boardDiv.style.gridTemplateColumns = `repeat(${L}, 50px)`;
   boardDiv.innerHTML = "";
 
-  for (let i = 0; i < L; i++) {
-    for (let j = 0; j < L; j++) {
+  let board;
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
       let cell = document.createElement("div");
       cell.classList.add("cell");
       cell.dataset.row = i;
       cell.dataset.col = j;
 
-      cell.addEventListener("click", () => {
+      cell.addEventListener("click", async () => {
+        if (isFirstClick) {
+          board = createBoard(size, numMines, i, j);
+          isFirstClick = false;
+        }
         showCell(i, j, board);
       });
 
       boardDiv.appendChild(cell);
     }
   }
-}
+};
 
 function showCell(i, j, board) {
   const cell = document.querySelector(
@@ -71,16 +94,12 @@ function showCell(i, j, board) {
   if (!cell || cell.classList.contains("revealed")) return;
 
   cell.classList.add("revealed");
-  cell.style.backgroundColor = "lightgray";
-  cell.style.color = "black";
+  defineCellProps(cell);
   cell.innerText = board[i][j] === 0 ? "" : board[i][j];
 
   if (board[i][j] === "M") {
     cell.classList.add("mine");
-    cell.innerText = "M";
-    cell.style.backgroundColor = "red";
-    cell.style.border = "1px solid #000";
-    cell.style.alignItems = "center";
+    defineCellProps(cell);
     showAllCells(board);
   } else if (board[i][j] === 0) {
     showCloseCells(i, j, board);
@@ -98,46 +117,39 @@ function showAllCells(tabuleiro) {
 
       if (cell && !cell.classList.contains("revealed")) {
         cell.classList.add("revealed");
-        cell.classList.remove("cell");
-        cell.style.backgroundColor = "lightgray";
-        cell.style.color = "black";
 
         if (tabuleiro[i][j] === "M") {
           cell.classList.add("mine");
-          cell.innerText = "M";
-          cell.style.backgroundColor = "red";
-          cell.style.border = "1px solid #000";
-          cell.style.alignItems = "center";
         } else if (tabuleiro[i][j] === 0) {
           cell.innerText = "";
         } else {
           cell.innerText = tabuleiro[i][j];
         }
       }
+      defineCellProps(cell);
     }
   }
 }
 
-function showCloseCells(i, j, tabuleiro) {
-  const isInBoardSize = (x, y) =>
-    x >= 0 && x < tabuleiro.length && y >= 0 && y < tabuleiro[x].length;
+function showCloseCells(i, j, board) {
+  const size = board.length;
 
-  showCell(i, j, tabuleiro);
+  showCell(i, j, board);
 
-  if (tabuleiro[i][j] === 0) {
+  if (board[i][j] === 0) {
     for (let dx = -1; dx <= 1; dx++) {
       for (let dy = -1; dy <= 1; dy++) {
         let newX = i + dx;
         let newY = j + dy;
 
         if (
-          isInBoardSize(newX, newY) &&
-          tabuleiro[newX][newY] !== "M" &&
+          isInBoardSize(newX, newY, size) &&
+          board[newX][newY] !== "M" &&
           !document
             .querySelector(`.cell[data-row='${newX}'][data-col='${newY}']`)
             .classList.contains("revealed")
         ) {
-          showCloseCells(newX, newY, tabuleiro);
+          showCloseCells(newX, newY, board);
         }
       }
     }
@@ -146,10 +158,9 @@ function showCloseCells(i, j, tabuleiro) {
 
 // nas configurações, vamos deixar no máximo um tabuleiro 20x20, 200 bombas
 // Testando a função
-const L = 10; // Tamanho do tabuleiro LxL
-const numMinas = 10; // Número de minas no tabuleiro
+const L = 20; // Tamanho do tabuleiro LxL
+const numMinas = 100; // Número de minas no tabuleiro
 
 document.addEventListener("DOMContentLoaded", () => {
-  let tabuleiro = createBoard(L, numMinas);
-  renderBoard(tabuleiro);
+  renderBoard(L, numMinas);
 });
