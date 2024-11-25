8e67aa4bbeb28fb656d570307814ac97a0044704
Francisca Rosa <f.rosa96@hotmail.com>
2024-11-25 20:33:27 -0300

    refactor: db conection

--- src/login.js
+++ src/login.js
@@ -9,59 +9,41 @@ document.querySelector("form").addEventListener("submit", function (event) {
   const password = document.getElementById("password").value;
 
   login(username, password);
-
-  /*const validUsername = "Pietra";
-  const validPassword = "123456";
-
-  // Verificação
-  if (username === validUsername && password === validPassword) {
-    window.location.href = "iniciar.html";
-  } else {
-    Swal.fire({
-      icon: "error",
-      title: "Erro!",
-      text: "Usuário ou senha incorretos!",
-    });
-  }
-});*/
 });
 
 function login(username, password) {
-
   let request = new XMLHttpRequest();
   let comando = `SELECT senha FROM usuario WHERE username = '${username}';`;
-  
+
   request.onreadystatechange = () => {
-
     if (request.readyState === XMLHttpRequest.DONE) {
+      if (request.status == 200) {
+        const passwordJson = JSON.parse(request.responseText);
 
-      if (request.status == 200)
-      {
-          const passwordJson = JSON.parse(request.responseText);
-         
-          if (password == passwordJson[0].senha)
-            window.location.href = "iniciar.html";
-          else
-          {
-            Swal.fire({
-              icon: "error",
-              title: "Erro!",
-              text: "Usuário ou senha incorretos!",
-            });
-          }
-      }
-      else
-      {
+        if (password == passwordJson[0].senha)
+          window.location.href = "iniciar.html";
+        else {
+          Swal.fire({
+            icon: "error",
+            title: "Erro!",
+            text: "Usuário ou senha incorretos!",
+          });
+        }
+      } else {
         Swal.fire({
           icon: "error",
           title: "Erro!",
           text: "Um erro ocorreu ao realizar o login",
         });
       }
-      
-    } 
+    }
   };
-  request.open('GET', 'http://localhost/minefield/src/php/consultar.php?comando=' + encodeURIComponent(comando), true);
-  request.send();
 
+  // o caminho é diferente a depender de onde está a pasta no xampp
+  const url =
+    "http://localhost/minefield/src/php/consultar.php?comando=" +
+    encodeURIComponent(comando);
+
+  request.open("GET", url, true);
+  request.send();
 }
